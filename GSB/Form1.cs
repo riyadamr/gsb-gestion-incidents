0c847f1a78e2c934d822f231d303bb8e705503b3
Amrani <AmraniRiyad@users.noreply.github.com>
2014-12-03 15:21:51 +0100

    Base

---
+++ GSB/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+
+namespace GSB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bddGSB gsb = new bddGSB();
+            gsb.connexionBase("ouvrir");
+                    MessageBox.Show("Connection Open ! ");
+
+
+ 
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ GSB/bddGSB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GSB
+{
+    class bddGSB
+    {
+        static private string paramConnexion = "Database=gsb;Data Source=localhost;User Id=root;Password=";
+        static MySqlConnection connexion = new MySqlConnection();
+        static connexion = new MySqlConnection(paramConnexion);
+        public void connexionBase(string connexionChoix)
+        {
+
+
+            if (connexionChoix == "ouvrir") { connexion.Open(); }
+            if (connexionChoix == "fermer") { connexion.Close(); }
+        }
+
+        // Requête de sélection des praticiens
+
+        //requete =
+        //    @"SELECT nomPraticien,prenomPraticien,
+        //   diplomePraticien,specialitePraticien 
+        //    FROM praticien ORDER BY nomPraticien,prenomPraticien;";
+        // MySqlCommand selectPraticien =
+        //     new MySqlCommand(requete, connexionBase);
+        // MySqlDataReader resultatsPraticien = 
+        //     selectPraticien.ExecuteReader();
+    }
+}
